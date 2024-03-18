7468ed4f10f9e7afc712439f531e4ef20079f922
Haitam ELIMAMI <haitamelimami1998@outlook.fr>
2024-03-18 20:47:58 +0000

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,14 +2,8 @@
 #include <stdio.h>
 
 void reverse(listint_t **head);
-int copare_lists(listint_t *head, listint_t *middle, int len);
-/**
- * is_palindrome - checks if singly linked list is a palindrome
- *
- * @head: pointer to pointer of the first node in the list passed
- *
- * return: 0 if a palindrome isnt detected and 1 if yes
- */
+int compare_lists(listint_t *head, listint_t *middle, int len); // Corrected function name
+
 int is_palindrome(listint_t **head)
 {
     int len, i;
@@ -17,68 +11,60 @@ int is_palindrome(listint_t **head)
     listint_t *middle;
 
     if (head == NULL || *head == NULL)
-      return (1);
+        return (1);
     tmp = *head;
     middle = *head;
 
     for (len = 0; tmp != NULL; len++)
-      tmp = tmp->next;
+        tmp = tmp->next;
 
-    len = len / 2
+    len = len / 2; // Added semicolon to end statement
 
     for (i = 1; i < len; i++)
-      middle = middle->next;
+        middle = middle->next;
     if (len % 2 != 0 && len != 1)
     {
-      middle = middle->next;
-      len = len - 1
+        middle = middle->next;
+        len = len - 1; // Added semicolon to end statement
     }
     reverse(&middle);
-    i = copare_lists(*head, middle, len);
+    i = compare_lists(*head, middle, len); // Corrected function name
 
     return (i);
 }
-/**
- * compare_lists - compare two lists
- * @head: pointer to the head node
- * @middle: pointer to the middle node
- * @len: length of the list
- * Return: if the same 1, if not 0
- */
-int compare_lists(listint_t head, listint_t *middle, int len)
+
+int compare_lists(listint_t *head, listint_t *middle, int len) // Corrected function name and parameters
 {
     int i;
 
     if (head == NULL || middle == NULL)
-      return (1);
+        return (1);
     for (i = 0; i < len; i++)
     {
-      if (head->n != middle->n)
-        return (0);
-      head = head->next;
-      middle = middle->next;
+        if (head->n != middle->n)
+            return (0);
+        head = head->next;
+        middle = middle->next;
     }
     return (1);
 }
-/**
- * reverse - reverse a list
- * @head: pointer to the head to reverse
- */
+
 void reverse(listint_t **head)
 {
     listint_t *current;
     listint_t *next;
-    listint_t *prev;
+    listint_t *prev = NULL; // Initialized prev to NULL
 
     if (head == NULL || *head == NULL)
-      return;
-    prev = NULL;
+        return;
+
     current = *head;
     while (current != NULL)
     {
-      next = current->next;
-      current->next = prev;
-      current = next;
+        next = current->next;
+        current->next = prev;
+        prev = current; // Moved prev to next position
+        current = next;
     }
     *head = prev;
 }
